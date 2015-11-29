d53c8fc3e0719de64e47087b92c6e32b4056922d
scottwey <wey.scott@gmail.com>
2015-11-29 14:03:21 -0800

    Finally working

--- RecyclingWorld/src/Tester.java
+++ RecyclingWorld/src/Tester.java
@@ -11,6 +11,8 @@ import java.io.FileNotFoundException;
 public class Tester {
 
     public static void main(String [] args) throws FileNotFoundException {
+        Statistics s = new Statistics();
+        s.makeTables();
         RecyclingDemo RD = new RecyclingDemo();
     }
 
